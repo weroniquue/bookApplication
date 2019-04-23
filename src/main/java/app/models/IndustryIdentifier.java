bd5da6c392aa5f84e5b33f91583a7df612bc11cd
A702385 <weronica97@gmail.com>
2019-04-23 15:18:44 +0200

    return book by isnb

--- src/main/java/app/models/IndustryIdentifier.java
+++ src/main/java/app/models/IndustryIdentifier.java
@@ -5,17 +5,17 @@ public class IndustryIdentifier
 	private String type;
 	private String identifier;
 	
-	private String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
 	public String getIdentifier() {
 		return identifier;
 	}
 	public void setIdentifier(String identifier) {
 		this.identifier = identifier;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
 	}
     
     
--- src/main/java/app/service/BookService.java
+++ src/main/java/app/service/BookService.java
@@ -2,11 +2,37 @@ package app.service;
 
 import java.util.Optional;
 
-import app.models.Book;
+import org.springframework.stereotype.Service;
 
+import app.models.Book;
+import app.models.IndustryIdentifier;
+import app.models.Item;
+import app.utils.JsonReader;
+
+@Service
 public class BookService {
 	
-	private Optional<Book> book;
+	public Optional<Item> getBookByIsbn(String isnb) {
+		Book books = new JsonReader().parseJson().get();
+		
+		return Optional.of(
+				books.getItems()
+				.stream()
+				.filter( (book) -> book.getId().equals(isnb))
+				.findAny()
+				.orElse(
+					books.getItems()
+					.stream()
+					.filter( book -> 
+							book.getVolumeInfo().getIndustryIdentifiers()
+							.stream()
+							.map(IndustryIdentifier::getIdentifier)
+							.anyMatch(identifier -> identifier.equals(isnb))
+					)
+					.findFirst().get()
+				));
+		
+	}
 	
 	
 
